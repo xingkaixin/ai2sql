be999b7fb12674db13d331dc7b98520a8fd31b05
XingKaiXin <xingkaixin@gmail.com>
2023-04-19 13:08:21 +0800

    feat: Add AI2Sql skill for querying MySQL databases.
    - Add `ai to sql` skill and configuration file
    - Include prompt for querying MySQL database and retrieving company information
    - New file `ai2sql/core/skill.py` added with models and configuration loading method
    - GPT-3 model used for completions with specified token count and temperature
    - `config.yaml` moved to `config` directory and added to `.gitignore`

---
+++ ai2sql/core/skill.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from typing import Optional
+
+import yaml
+from pydantic import BaseModel
+
+
+class Completion(BaseModel):
+    model_name: Optional[str] = "gpt-3.5-turbo"
+    max_token: Optional[int] = 1024
+    temperature: Optional[float] = 0.5
+    stream: Optional[bool] = False
+    stop: Optional[list[str]]
+
+
+class Config(BaseModel):
+    description: str
+    completion: Completion
+    prompt: str
+
+    @classmethod
+    def load_skill(cls, path: Path):
+        yaml_path = path.resolve().joinpath("config.yaml")
+        txt_path = path.resolve().joinpath("prompt.txt")
+        data_config = yaml.safe_load(yaml_path.read_text("utf-8"))
+        data_txt = txt_path.read_text("utf-8")
+        return cls(
+            description=data_config["description"],
+            completion=Completion(**data_config["completion"]),
+            prompt=data_txt,
+        )
+
+
+def load_skill(path: str):
+    config = Config.load_skill(Path(path)).dict()
+    return config
