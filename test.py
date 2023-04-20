48ff911da3cb5237ca29ea54d6b17c6a490c49ed
XingKaiXin <xingkaixin@gmail.com>
2023-04-20 17:55:25 +0800

    refactor: Refactor test file for improved functionality.
    - Implement main function in test.py
    - Clean up unused variables and imports in test.py
    - Update various files with minor changes and improvements
    - Omit large changes in some files from diff summary
    - Improve code readability and organization throughout project

--- test.py
+++ test.py
@@ -4,29 +4,31 @@ from ai2sql.react import ReActAgent
 from ai2sql.schemas.message import AIMessage, SystemMessage, UserMessage
 from ai2sql.schemas.template import SystemMessageTemplate, UserMessageTemplate
 
-sys_template = "a {a} {c}"
-user_template = "b {d}"
-sys = SystemMessageTemplate.from_template(sys_template)
-user = UserMessageTemplate.from_template(user_template)
+
+def main():
+    sys_template = "a {a} {c}"
+    user_template = "b {d}"
+    sys = SystemMessageTemplate.from_template(sys_template)
+    user = UserMessageTemplate.from_template(user_template)
+
+    sys_msg = SystemMessage(content="system message test")
+    sys_prompt = sys.format_prompt(a="aa", c="ccc")
+
+    chat_config = load_skill("AI2Sql")
+
+    agent = ReActAgent(prompt=chat_config.prompt)
+    agent.run()
+    sys_msg = SystemMessage(content=agent.prompt)
+    usr_msg = UserMessage(content="查询上市公司最新股本")
+
+    chat = ChatOpenAI(
+        **chat_config.completion.dict(exclude={"stop"}),
+    )
+
+    answer = chat([sys_msg, usr_msg], stop=chat_config.completion.stop)
+
+    print(answer)
 
 
-sys_msg = SystemMessage(content="system message test")
-sys_prompt = sys.format_prompt(a="aa", c="ccc")
-
-
-chat_config = load_skill("AI2Sql")
-
-
-agent = ReActAgent(prompt=chat_config.prompt)
-agent.run()
-sys_msg = SystemMessage(content=agent.prompt)
-usr_msg = UserMessage(content="查询上市公司最新股本")
-
-
-chat = ChatOpenAI(
-    **chat_config.completion.dict(exclude={"stop"}),
-)
-
-answer = chat([sys_msg, usr_msg], stop=chat_config.completion.stop)
-
-print(answer)
+if __name__ == "__main__":
+    main()
